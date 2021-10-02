62fcd70b19b613a3c5fb1ecea663c1d9107f9648
andrequera <andrequera@gmail.com>
2021-10-02 17:00:51 -0300

    buscador filtrando por nombre

--- src/store/flux.js
+++ src/store/flux.js
@@ -6,14 +6,15 @@ const getState = ({ getStore, getActions, setStore }) => {
             price: "",
             discount: "",
             category: "",
-            products: null
+            products: null,
+            buscador: ""
         },
 
         actions: {
-
+            // Traigo los productos del back por medio del .then, luego los muestro en el componente respectivo
 
             getProduct: () => {
-                
+
                 fetch("http://localhost:8080/api/products")
                     .then(respuesta => respuesta.json())
                     .then(data => setStore({ products: data }))
@@ -22,7 +23,30 @@ const getState = ({ getStore, getActions, setStore }) => {
 
             },
 
+            capturaCampos: (e) => {
 
+                const store = getStore();
+                console.log(e.target.value, "campos del buscador inicial")
+
+                setStore({ ...store, [e.target.name]: e.target.value })
+            },
+
+
+            enviarFormulario: (e) => {
+                //para que no se recargue cada que le doy enter en un input
+                e.preventDefault()
+                const store = getStore();
+                
+                console.log(store.buscador, "lo que busca el usuario en el back")
+
+                fetch("http://localhost:8080/api/products?name="+ store.buscador)
+                .then(respuesta => respuesta.json())
+                .then(data => setStore({ products: data }))
+                .catch(error => console.log(error))
+
+
+
+            }
 
         },
     };
--- src/view/shop.js
+++ src/view/shop.js
@@ -1,20 +1,21 @@
 import React, { useContext, useState, useEffect } from 'react'
 import ProductCard from '../components/ProductCard';
+import { Context } from "../store/appContext"
 
 const Shop = () => {
-    return (<>
+const { store, actions } = useContext(Context)
+return (<>
         <div className="container">
             <div className="row  py-4">
 
-                <nav className="center-navbar navbar-light bg-light">
-                    <div className="container-fluid">
-                        <form className="d-flex">
-                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"></input>
-                            <a className="btn btn-outline-success" type="submit"><img src={"../img/lupadebuscar" + ".PNG"} width="30" height="30" className="rounded img-fluid" alt="...imagen..."></img></a>
+                
+                        <form className="d-flex" onSubmit={e => actions.enviarFormulario(e)}>
+                        {/* onsubmit captura los campos con el enter y los envia al back */}
+                            <input className="form-control me-2" name="buscador" type="search" placeholder="Search" aria-label="Search" onChange={(e) => actions.capturaCampos(e)}></input>
+                            
+                            <button className="btn btn-outline-success" type="submit"><img src={"../img/lupadebuscar" + ".PNG"} width="30" height="30" className="rounded img-fluid" alt="...imagen..."></img></button>
                         </form>
-                    </div>
-                </nav>
-
+                   
             </div>
 
             <div className="row">
